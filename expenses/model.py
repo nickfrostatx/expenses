b691fa577b232eefd92a718e679dc3214df01007
Nick Frost <nickfrostatx@gmail.com>
2015-12-14 18:12:51 -0600

    Add purchase model

--- expenses/model.py
+++ expenses/model.py
@@ -13,3 +13,12 @@ class User(db.Model):
     name = db.Column(db.String(254))
     username = db.Column(db.String(254), unique=True)
     password = db.Column(db.String(60))
+    purchases = db.relationship('Purchase', backref='user')
+
+
+class Purchase(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(254))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    cost = db.Column(db.Integer)
