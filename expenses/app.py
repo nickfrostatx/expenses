b031c1d6d2a4e6214bf53357dd400c0d54b1c099
Nick Frost <nickfrostatx@gmail.com>
2015-12-16 00:12:17 -0600

    Use StrictRedis's from_url()

--- expenses/app.py
+++ expenses/app.py
@@ -2,7 +2,7 @@
 """Flask application factory."""
 
 from flask import Flask
-from redis import from_url
+from redis import StrictRedis
 from . import __name__ as package_name
 import os
 
@@ -18,7 +18,7 @@ def create_app():
 
     @app.before_first_request
     def init_db():
-        app.redis = from_url(app.config['REDIS_URL'])
+        app.redis = StrictRedis.from_url(app.config['REDIS_URL'])
 
     from .error import register_error_handler, html_handler
     register_error_handler(app, html_handler)
