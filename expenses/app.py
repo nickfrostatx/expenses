9a2433ca82719620e51513ac52195cec768c5467
Nick Frost <nickfrostatx@gmail.com>
2015-12-15 23:04:05 -0600

    Do some redis config

--- expenses/app.py
+++ expenses/app.py
@@ -2,7 +2,7 @@
 """Flask application factory."""
 
 from flask import Flask
-from redis import StrictRedis
+from redis import from_url
 from . import __name__ as package_name
 import os
 
@@ -11,11 +11,14 @@ def create_app():
     """Return an instance of the main Flask application."""
     app = Flask(package_name)
 
-    # TODO: do some config
-    app.redis = StrictRedis()
+    app.config.setdefault('REDIS_URL', 'redis://localhost')
 
     from .model import db
     db.init_app(app)
+
+    @app.before_first_request
+    def init_db():
+        app.redis = from_url(app.config['REDIS_URL'])
 
     from .error import register_error_handler, html_handler
     register_error_handler(app, html_handler)
